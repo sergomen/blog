c6123be4a70e4c21f5f571af86094d50c308d91a
Sergei Iugai <serg_omen@mail.ru>
2026-02-22 23:25:06 +0300

    fix [BLOG-9]: fixed file paths

--- app/page.js
+++ app/page.js
@@ -5,12 +5,15 @@ import matter from "gray-matter";
 import Link from "next/link";
 
 async function getPosts() {
-  const entries = await readdir("./public/", { withFileTypes: true });
+  const publicDir = path.join(process.cwd(), 'public');
+
+  const entries = await readdir(publicDir, { withFileTypes: true });
   const dirs = entries
     .filter((entry) => entry.isDirectory())
     .map((entry) => entry.name);
+
   const fileContents = await Promise.all(
-    dirs.map((dir) => readFile("./public/" + dir + "/index.md", "utf8")),
+    dirs.map((dir) => readFile(path.join(publicDir, dir, "index.md"), "utf8")),
   );
   const posts = dirs.map((slug, i) => {
     const fileContent = fileContents[i];
