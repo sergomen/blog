8003068fc6d62b9ff1ddc9cdd8f9bba567886dcf
Sergei Iugai <serg_omen@mail.ru>
2026-02-25 21:51:37 +0300

    feat [BLOG-15]: added typescript to the project; moved all files to src folder; fixed tailwindcss config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { readdir, readFile } from "node:fs/promises";
 import matter from "gray-matter";
 import Link from "next/link";
 import path from 'path';
+import { PostData } from "../types";
 
 async function getPosts() {
   const publicDir = path.join(process.cwd(), 'public');
@@ -15,13 +16,14 @@ async function getPosts() {
 
   const fileContents = await Promise.all(
     dirs.map((dir) => readFile(path.join(publicDir, dir, "index.md"), "utf8")),
-    // dirs.map((dir) => readFile(`${publicDir}/${dir}/index.md`, "utf8")),
   );
-  const posts = dirs.map((slug, i) => {
+
+  const posts: PostData[] = dirs.map((slug, i) => {
     const fileContent = fileContents[i];
     const { data } = matter(fileContent);
-    return { slug, ...data };
+    return { slug, title: data.title, date: data.date }
   });
+
   posts.sort((a, b) => {
     return Date.parse(a.date) < Date.parse(b.date) ? 1 : -1;
   });
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface PostData {
+  slug: string;
+  title: string;
+  date: string;
+}
